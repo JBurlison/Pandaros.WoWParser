cd10841abe9ee0252366af36f626d713f55d21b9
Jim Burlison <JBurlison@Gmail.com>
2021-11-10 19:27:37 -0500

    started raid stuff

---
+++ src/Pandaros.WoWParser.API/DomainModels/WoWRaid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pandaros.WoWParser.API.DomainModels
+{
+    public class WoWRaid
+    {
+        public string RaidId { get; set; }
+        public string GuildId { get; set; }
+        public string RaidName { get; set; }
+        public List<string> CharacterIds { get; set; } = new List<string>();
+        public List<string> GuildIds { get; set; } = new List<string>();
+    }
+}
---
+++ src/Pandaros.WoWParser.API/DomainModels/WoWRaidInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pandaros.WoWParser.API.DomainModels
+{
+    public class WoWRaidInfo
+    {
+        public string Name { get; set; }
+    }
+}
