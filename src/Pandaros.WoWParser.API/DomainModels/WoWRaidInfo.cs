7dcb4612d293c7499e051124f142e53e90a975e0
Jim Burlison <JBurlison@Gmail.com>
2021-11-10 19:33:35 -0500

    comments

--- src/Pandaros.WoWParser.API/DomainModels/WoWRaidInfo.cs
+++ src/Pandaros.WoWParser.API/DomainModels/WoWRaidInfo.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace Pandaros.WoWParser.API.DomainModels
 {
+    /// <summary>
+    ///     Metadata about the raid, sucha as name and boss fights
+    /// </summary>
     public class WoWRaidInfo
     {
         public string Name { get; set; }
