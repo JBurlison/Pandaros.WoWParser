772c9e4dd53345530efd791429d44f6e0b64a0fb
Jim Burlison <JBurlison@Gmail.com>
2021-11-11 00:42:24 -0500

    removed apixml

--- src/Pandaros.WoWParser.API/DomainModels/WoWInstance.cs
+++ src/Pandaros.WoWParser.API/DomainModels/WoWInstance.cs
@@ -13,6 +13,9 @@ namespace Pandaros.WoWParser.API.DomainModels
         public DateTime EndTime { get; set; }
         public List<string> CharacterIds { get; set; } = new List<string>();
         public List<string> GuildIds { get; set; } = new List<string>();
+        /// <summary>
+        ///  In order of fights that happend.
+        /// </summary>
         public List<string> FightIds { get; set; } = new List<string>();
     }
 }
